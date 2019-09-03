5ce76ca0ecf81c9374e2b8f4f2244a2684102c66
the-akira <gabrielfelippe90@gmail.com>
2019-09-03 17:08:18 -0300

    app models

--- projeto/__init__.py
+++ projeto/__init__.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from projeto.config import Config
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
 
 def create_app(config_class=Config):
 	app = Flask(__name__)
 	app.config.from_object(Config)
 
 	db.init_app(app)
+
+	bcrypt.init_app(app)
+	login_manager.init_app(app)
 
 	from projeto.main.routes import main
 	app.register_blueprint(main)
---
+++ projeto/models.py
@@ -0,0 +1,41 @@
+from projeto import db, login_manager
+from flask import current_app
+from datetime import datetime
+from flask_login import UserMixin
+
+# >>> from projeto import create_app
+# >>> app = create_app()
+# >>> app.app_context().push()
+# >>> from projeto.models import User
+# >>> from projeto import db
+# >>> db.create_all()
+# >>> user_1 = User(username='akira', email='akira@gmail.com', password='akira123')
+# >>> db.session.add(user_1)
+# >>> db.session.commit()
+# >>> User.query.all()
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	image_file = db.Column(db.String(20), default='default.jpg')
+	password = db.Column(db.String(60), nullable=False)
+	Book = db.relationship('Book', backref='user', lazy=True)
+
+	def __repr__(self):
+		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Book(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100), nullable=False)
+	author = db.Column(db.String(100), nullable=False)
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	summary = db.Column(db.Text, nullable=False)	
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+	def __repr__(self):
+		return f"Book('{self.title}', '{self.date_posted}')"
