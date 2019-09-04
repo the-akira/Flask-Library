33876065491422ef30841061df585690f0d34b3c
the-akira <gabrielfelippe90@gmail.com>
2019-09-04 20:25:26 -0300

    update book funcionality

--- projeto/books/routes.py
+++ projeto/books/routes.py
@@ -27,3 +27,39 @@ def new_book():
 def book(book_id):
 	book = Book.query.get_or_404(book_id)
 	return render_template('book.html', book=book)
+
+@books.route("/book/<int:book_id>/delete", methods=["POST"])
+@login_required
+def delete_book(book_id):
+	book = Book.query.get_or_404(book_id)
+	if book.user != current_user:
+		abort(403)
+	db.session.delete(book)
+	db.session.commit()
+	flash('Your book has been deleted', 'success')
+	return redirect(url_for('main.home'))
+
+@books.route("/book/<int:book_id>/update", methods=["GET", "POST"])
+@login_required
+def update_book(book_id):
+	book = Book.query.get_or_404(book_id)
+	if book.user != current_user:
+		abort(403)
+	form = BookForm()
+	if form.validate_on_submit():
+		if form.image_book.data:
+			picture_file = save_book_picture(form.image_book.data)
+			book.title = form.title.data
+			book.author = form.author.data
+			book.summary = form.summary.data 
+			book.image_book = picture_file
+			db.session.commit()
+			flash('Your book has been updated', 'success')
+			return redirect(url_for('books.book', book_id=book.id))
+	elif request.method == 'GET':
+		# Preenche os formulários com os dados atuais do post
+		form.title.data = book.title
+		form.author.data = book.author
+		form.summary.data = book.summary
+		form.image_book.data = book.image_book
+	return render_template('create_book.html', form=form)
